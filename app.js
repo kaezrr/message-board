d47b4751c94c31a9b453372f2be6988d1d4c9303
Anjishnu Banerjee <inviktor27@gmail.com>
2025-03-24 22:06:20 +0530

    Add index page

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 import express from "express";
+import path from "node:path";
+import indexRouter from "./routes/indexRouter.js";
 
 const app = express();
+
+app.set("views", path.join(path.resolve(), "views"));
+app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`App is listening at port ${PORT}!`);
+});
